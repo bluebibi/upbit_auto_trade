9964c3395cdfa46532820f0d1ca7a4601a2a3426
Youn-Hee Han <yh21.han@gmail.com>
2019-07-25 23:39:26 +0900

    s

--- scripts/upbit_record_ubuntu.sh
+++ scripts/upbit_record_ubuntu.sh
@@ -3,5 +3,5 @@
 cd $HOME/git/upbit_auto_trade
 
 /Users/yhhan/anaconda3/envs/upbit_auto_trade/bin/python -m upbit.upbit_recorder
-/Users/yhhan/anaconda3/envs/upbit_auto_trade/bin/python -m predict.buy
+#/Users/yhhan/anaconda3/envs/upbit_auto_trade/bin/python -m predict.buy
 
