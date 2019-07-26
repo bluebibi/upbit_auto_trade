ca12a5bc47454b0f45a2c72561cfd89449710672
Youn-Hee Han <yh21.han@gmail.com>
2019-07-26 13:18:11 +0900

    das

--- scripts/make_models_mac.sh
+++ scripts/make_models_mac.sh
@@ -2,5 +2,5 @@
 #
 cd $HOME/git/upbit_auto_trade
 
-/Users/yhhan/anaconda3/envs/upbit_auto_trade/bin/python -m predict.main_prediction >> /Users/yhhan/git/upbit_auto_trade/logs/make_models.log 2>&1
+/Users/yhhan/anaconda3/envs/upbit_auto_trade/bin/python -m predict.make_models >> /Users/yhhan/git/upbit_auto_trade/logs/make_models.log 2>&1
 
