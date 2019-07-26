4ff8642b8cb37fe8c4a65a6a70e0d496251ae721
Youn-Hee Han <yh21.han@gmail.com>
2019-07-26 11:40:20 +0900

    ...

--- scripts/main_prediction_ubuntu.sh
+++ scripts/main_prediction_ubuntu.sh
@@ -2,5 +2,5 @@
 #
 cd $HOME/git/upbit_auto_trade
 
-/home/yhhan/anaconda3/envs/upbit_auto_trade/bin/python -m predict.main_prediction >> /home/yhhan/git/upbit_auto_trade/predict/logs/main_prediction_ubuntu.log 2>&1
+/home/yhhan/anaconda3/envs/upbit_auto_trade/bin/python -m predict.main_prediction >> /home/yhhan/git/upbit_auto_trade/logs/main_prediction_ubuntu.log 2>&1
 
--- scripts/upbit_record_ubuntu.sh
+++ scripts/upbit_record_ubuntu.sh
@@ -2,7 +2,7 @@
 #
 cd $HOME/git/upbit_auto_trade
 
-/home/yhhan/anaconda3/envs/upbit_auto_trade/bin/python -m upbit.upbit_recorder >> /home/yhhan/git/upbit_auto_trade/predict/logs/upbit_record_ubuntu.log 2>&1
+/home/yhhan/anaconda3/envs/upbit_auto_trade/bin/python -m upbit.upbit_recorder >> /home/yhhan/git/upbit_auto_trade/logs/upbit_record_ubuntu.log 2>&1
 
 #/Users/yhhan/anaconda3/envs/upbit_auto_trade/bin/python -m predict.buy
 
