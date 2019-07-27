d48b3066cc2611f912177d24bd4f94339122399f
Youn-Hee Han <yh21.han@gmail.com>
2019-07-28 05:52:39 +0900

    das

--- db/statistics.py
+++ db/statistics.py
@@ -15,6 +15,9 @@ else:
 
 select_all_buy_sell_sql = "SELECT * FROM BUY_SELL ORDER BY id DESC;"
 
+select_one_record_KRW_BTC_sql = "SELECT datetime FROM KRW_BTC ORDER BY id DESC LIMIT 1;"
+
+count_rows_KRW_BTC_sql = "SELECT * FROM KRW_BTC ORDER BY id DESC LIMIT 1;"
 
 def render_template(**kwargs):
     templateLoader = jinja2.FileSystemLoader(searchpath="db")
@@ -23,47 +26,67 @@ def render_template(**kwargs):
     return templ.render(**kwargs)
 
 
+def get_KRW_BTC_info():
+    with sqlite3.connect(sqlite3_db_filename, timeout=10, isolation_level=None, check_same_thread=False) as conn:
+        cursor = conn.cursor()
+
+        rows = cursor.execute(select_one_record_KRW_BTC_sql)
+        for row in rows:
+            last_krw_btc_datetime = row[0]
+        conn.commit()
+
+        rows = cursor.execute(count_rows_KRW_BTC_sql)
+        for row in rows:
+            num_krw_btc_records = row[0]
+        conn.commit()
+
+    return last_krw_btc_datetime, num_krw_btc_records
+
+
 def buy_sell_tables():
     with sqlite3.connect(sqlite3_db_filename, timeout=10, isolation_level=None, check_same_thread=False) as conn:
         cursor = conn.cursor()
 
         rows = cursor.execute(select_all_buy_sell_sql)
 
-        txt = "<tr><th>매수 기준 날짜/시각</th><th>구매 코인</th><th>모델 확신도<br/>(CNN | LSTM)</th><th>구매 가격</th>"
-        txt += "<th>현재 가격</th><th>경과 시간</th><th>등락 비율</th><th>상태</th></tr>"
-        total_rate = 0.0
-        num = 0
-        num_success = 0
-        num_trail_bought = 0
-        num_gain = 0
-        num_loss = 0
-        for row in rows:
-            num += 1
-            if row[9] == CoinStatus.success_sold.value:
-                num_success += 1
-            elif row[9] == CoinStatus.gain_sold.value:
-                num_gain += 1
-            elif row[9] == CoinStatus.loss_sold.value:
-                num_loss += 1
-            elif row[9] == CoinStatus.trailed.value:
-                num_trail_bought += 1
-            elif row[9] == CoinStatus.bought.value:
-                num_trail_bought += 1
+        conn.commit()
 
-            total_rate += float(row[8])
-            txt += "<tr>"
-            txt += "<td>{0}</td><td>{1}</td><td>{2} | {3}</td><td>{4}</td><td>{5}</td><td>{6}</td><td>{7}%</td><td>{8}</td>".format(
-                row[2].replace(":00", ""),
-                row[1],
-                convert_unit_2(row[3]),
-                convert_unit_2(row[4]),
-                row[5],
-                row[7],
-                elapsed_time_str(row[2], row[6]),
-                convert_unit_2(row[8] * 100),
-                coin_status_to_hangul(row[9])
-            )
-            txt += "</tr>"
+    txt = "<tr><th>매수 기준 날짜/시각</th><th>구매 코인</th><th>모델 확신도<br/>(CNN | LSTM)</th><th>구매 가격</th>"
+    txt += "<th>현재 가격</th><th>경과 시간</th><th>등락 비율</th><th>상태</th></tr>"
+    total_rate = 0.0
+    num = 0
+    num_success = 0
+    num_trail_bought = 0
+    num_gain = 0
+    num_loss = 0
+
+    for row in rows:
+        num += 1
+        if row[9] == CoinStatus.success_sold.value:
+            num_success += 1
+        elif row[9] == CoinStatus.gain_sold.value:
+            num_gain += 1
+        elif row[9] == CoinStatus.loss_sold.value:
+            num_loss += 1
+        elif row[9] == CoinStatus.trailed.value:
+            num_trail_bought += 1
+        elif row[9] == CoinStatus.bought.value:
+            num_trail_bought += 1
+
+        total_rate += float(row[8])
+        txt += "<tr>"
+        txt += "<td>{0}</td><td>{1}</td><td>{2} | {3}</td><td>{4}</td><td>{5}</td><td>{6}</td><td>{7}%</td><td>{8}</td>".format(
+            row[2].replace(":00", ""),
+            row[1],
+            convert_unit_2(row[3]),
+            convert_unit_2(row[4]),
+            row[5],
+            row[7],
+            elapsed_time_str(row[2], row[6]),
+            convert_unit_2(row[8] * 100),
+            coin_status_to_hangul(row[9])
+        )
+        txt += "</tr>"
 
     return txt, convert_unit_2(total_rate * 100), num, num_trail_bought, num_success, num_gain, num_loss
 
@@ -75,6 +98,8 @@ def main():
 
     buy_sell_text, total_rate, num, num_trail_bought, num_success, num_gain, num_loss = buy_sell_tables()
 
+    last_krw_btc_datetime, num_krw_btc_records = get_KRW_BTC_info()
+
     html_data = render_template(
         buy_sell_text=buy_sell_text,
         total_rate=total_rate,
@@ -82,7 +107,9 @@ def main():
         num_trail_bought=num_trail_bought,
         num_success=num_success,
         num_gain=num_gain,
-        num_loss=num_loss
+        num_loss=num_loss,
+        last_krw_btc_datetime=last_krw_btc_datetime,
+        num_krw_btc_records=num_krw_btc_records
     )
 
     msg = MIMEText(html_data, _subtype="html", _charset="utf-8")
