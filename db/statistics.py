6a92eca1f2ef379a32ddfbd93afa6329a797c75f
Youn-Hee Han <yh21.han@gmail.com>
2019-07-28 05:13:16 +0900

    ...

--- db/statistics.py
+++ db/statistics.py
@@ -29,13 +29,31 @@ def buy_sell_tables():
 
         rows = cursor.execute(select_all_buy_sell_sql)
 
-        txt = "<tr><th>매수 날짜/시각</th><th>구매 코인</th><th>CNN 확신도</th><th>LSTM 확신도</th><th>구매 가격</th>"
+        txt = "<tr><th>매수 기준 날짜/시각</th><th>구매 코인</th><th>모델 확신도<br/>(CNN | LSTM)</th><th>구매 가격</th>"
         txt += "<th>현재 가격</th><th>날짜/시각</th><th>등락 비율</th><th>상태</th></tr>"
         total_rate = 0.0
+        num = 0
+        num_success = 0
+        num_trail = 0
+        num_gain = 0
+        num_loss = 0
+        num_bought = 0
         for row in rows:
+            num += 1
+            if row[9] == CoinStatus.success_sold.value:
+                num_success += 1
+            elif row[9] == CoinStatus.gain_sold.value:
+                num_gain += 1
+            elif row[9] == CoinStatus.loss_sold.value:
+                num_loss += 1
+            elif row[9] == CoinStatus.trailed.value:
+                num_trail += 1
+            elif row[9] == CoinStatus.bought.value:
+                num_bought += 1
+
             total_rate += float(row[8])
             txt += "<tr>"
-            txt += "<td>{0}</td><td>{1}</td><td>{2}</td><td>{3}</td><td>{4}</td><td>{5}</td><td>{6}</td><td>{7}</td><td>{8}</td>".format(
+            txt += "<td>{0}</td><td>{1}</td><td>{2} | {3}</td><td>{4}</td><td>{5}</td><td>{6}</td><td>{7}</td><td>{8}</td>".format(
                 row[2],
                 row[1],
                 convert_unit_2(row[3]),
@@ -48,7 +66,7 @@ def buy_sell_tables():
             )
             txt += "</tr>"
 
-    return txt, convert_unit_4(total_rate)
+    return txt, convert_unit_2(total_rate * 100), num, num_bought, num_trail, num_success, num_gain, num_loss
 
 
 def main():
@@ -56,9 +74,18 @@ def main():
     s.starttls()
     s.login('yh21.han@gmail.com', GOOGLE_APP_PASSWORD)
 
-    buy_sell_text, total_rate = buy_sell_tables()
+    buy_sell_text, total_rate, num, num_bought, num_trail, num_success, num_gain, num_loss = buy_sell_tables()
 
-    html_data = render_template(buy_sell_text=buy_sell_text, total_rate=total_rate)
+    html_data = render_template(
+        buy_sell_text=buy_sell_text,
+        total_rate=total_rate,
+        num=num,
+        num_bought=num_bought,
+        num_success=num_success,
+        num_gain=num_gain,
+        num_loss=num_loss,
+        num_trail=num_trail
+    )
 
     msg = MIMEText(html_data, _subtype="html", _charset="utf-8")
     msg['Subject'] = 'Statistics'
