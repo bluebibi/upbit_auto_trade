73a5b40be472fbfdb1bdf9bd163c63b63e4dd0b6
Youn-Hee Han <yh21.han@gmail.com>
2019-07-28 23:47:53 +0900

    dasdasdsa

--- tests/test_slack.py
+++ tests/test_slack.py
@@ -2,6 +2,8 @@ import os
 import sys
 import unittest
 
+from common.global_variables import SLACK_WEBHOOK_URL_1, SLACK_WEBHOOK_URL_2
+
 idx = os.getcwd().index("upbit_auto_trade")
 PROJECT_HOME = os.getcwd()[:idx] + "upbit_auto_trade/"
 sys.path.append(PROJECT_HOME)
@@ -11,7 +13,7 @@ from upbit.slack import PushSlack
 
 class TestSlack(unittest.TestCase):
     def setUp(self):
-        self.pusher = PushSlack()
+        self.pusher = PushSlack(SLACK_WEBHOOK_URL_1, SLACK_WEBHOOK_URL_2)
 
     def test_send_message(self):
         self.pusher.send_message("me", "<https://www.bithumb.com/trade/order/PPT|This> is the 테스트 msg")
