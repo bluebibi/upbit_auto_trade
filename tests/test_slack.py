4e2e392a9d0b0110202f2b1013fabea3e97bc59b
Youn-Hee Han <yh21.han@gmail.com>
2019-07-28 23:41:22 +0900

    das

--- tests/test_slack.py
+++ tests/test_slack.py
@@ -1,11 +1,12 @@
 import os
 import sys
 import unittest
-from upbit.slack import PushSlack
 
 idx = os.getcwd().index("upbit_auto_trade")
 PROJECT_HOME = os.getcwd()[:idx] + "upbit_auto_trade/"
 sys.path.append(os.getcwd())
+
+from upbit.slack import PushSlack
 
 
 class TestSlack(unittest.TestCase):
