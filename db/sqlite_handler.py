c2f2cccaf8504be6f39a57feb5d9085590187c4d
Youn-Hee Han <yh21.han@gmail.com>
2019-07-29 23:28:34 +0900

    dasdasd

--- db/sqlite_handler.py
+++ db/sqlite_handler.py
@@ -3,7 +3,7 @@ import sqlite3
 import sys, os
 idx = os.getcwd().index("upbit_auto_trade")
 PROJECT_HOME = os.getcwd()[:idx] + "upbit_auto_trade/"
-sys.path.append(os.getcwd())
+sys.path.append(PROJECT_HOME)
 
 from common.global_variables import *
 
